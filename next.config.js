78c3f68054519c699287fd9ef93c53b7ae2235f0
David <david@Davids-MacBook-Pro.local>
2022-09-17 11:38:06 +0900

    Change proxy domain

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*'
+        destination: 'https://amazon-clone-backend-two.vercel.app/api/:path*'
       }
     ]
   }
